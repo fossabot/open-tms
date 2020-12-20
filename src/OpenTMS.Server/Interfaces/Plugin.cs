6d8e4efef30fedc40c9518bd724928f29bd04ef7
Seth Murphy <3085408+eagerestwolf@users.noreply.github.com>
2020-12-19 22:50:55 -0500

    chore: push initial sources...finally. no code yet though :(

---
+++ src/OpenTMS.Server/Interfaces/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenTMS.Server.Interfaces
+{
+    public abstract class Plugin
+    {
+        /// <summary>
+        /// This property represents the name of the plugin
+        /// </summary>
+        public abstract string Name { get; }
+
+        /// <summary>
+        /// This property represents the author of the plugin
+        /// </summary>
+        public abstract string Author { get; }
+
+        /// <summary>
+        /// The website a user can visit for more information about the plugin.
+        /// </summary>
+        public abstract string Website { get; }
+
+        /// <summary>
+        /// A link to the repository for this plugin.
+        /// </summary>
+        public abstract string Repository { get; }
+
+        /// <summary>
+        /// The version of the plugin. This will be used during update checks
+        /// </summary>
+        public abstract string Version { get; }
+    }
+}
